082ac400183ca5b619926b170f8ab82d9777fbfd
samuel3740 <142777707+samuel3740@users.noreply.github.com>
2025-01-16 09:34:56 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,25 +1,22 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
+int main(){
     int i, j, N, M;
     cin >> N >> M;
-
     vector<int> arr(N);
-
-    for (int n = 0; n < N; ++n){
-        arr[n] = n + 1;
+    
+    for(int u = 0; u < N; ++u){
+        arr[u] = u + 1;
     }
-
-    for (int n = 0; n < M; ++n)
-    {
+    
+    for(int k = 0; k < M; ++k){
         cin >> i >> j;
         reverse(arr.begin() + i - 1, arr.begin() + j);
     }
-
-    for (int n = 0; n < N; ++n){
-        cout << arr[n] << " ";
-    }
+    for(int u = 0; u < N; ++u){
+        cout << arr[u] << " ";
+    }    
 }
