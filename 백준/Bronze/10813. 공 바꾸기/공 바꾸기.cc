078d56df5d2ea7a6f736f44c5fc7ca047158421a
samuel3740 <142777707+samuel3740@users.noreply.github.com>
2025-01-20 16:58:31 +0900

    공 바꾸기.cc

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -1,23 +1,26 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
- 
+
 int main(){
     int N, M;
     cin >> N >> M;
-    vector<int> arr(N+1);
- 
-    for (int i = 1; i < N + 1; ++i){
-        arr[i] = i;
+    
+    vector<int> arr(N);
+    
+    for (int i = 0; i < N; ++i){
+        arr[i] = i + 1;
     }
- 
-    for (int u = 0; u < M; ++u){
+    
+    for (int n = 0; n < M; ++n){
         int i, j;
         cin >> i >> j;
-        swap(arr[i], arr[j]);
+        
+        swap(arr[i - 1], arr[j - 1]);
     }
- 
-    for (int i = 1; i < N + 1; ++i){
-        cout << arr[i] << " ";
+    
+    for (int n = 0; n < N; ++n){
+        cout << arr[n] << " ";
     }
+    cout << endl;
 }
