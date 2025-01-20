95e6a4e0691137032321c1ec0757ed2509fecba6
samuel3740 <142777707+samuel3740@users.noreply.github.com>
2025-01-20 17:47:04 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -5,21 +5,15 @@ int main(){
     int N;
     cin >> N;
 
-    for(int i = 0; i < N; ++i){
-        for(int j = 0; j < N - i - 1; ++j){
-            cout << " ";
-        }
-        for(int j = 0; j < 2 * i + 1; ++j){
-            cout << "*";
-        }
-        cout << endl;
-    }
+    for(int i = 0; i < 2 * N - 1; ++i){
+        int stars = i < N ? 2 * i + 1 : 2 * (2 * N - i - 2) + 1;
+        int spaces = i < N ? N - i - 1 : i - N + 1;
 
-    for(int i = N - 2; i >= 0; --i){
-        for(int j = 0; j < N - i - 1; ++j){
+        for(int j = 0; j < spaces; ++j){
             cout << " ";
         }
-        for(int j = 0; j < 2 * i + 1; ++j){
+        
+        for(int j = 0; j < stars; ++j){
             cout << "*";
         }
         cout << endl;
