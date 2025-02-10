9abfbd3b3f4e0131f16484e899f4e968bd06047b
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-02-10 13:44:05 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -2,28 +2,19 @@
 using namespace std;
 
 int Fibonacci(int num){
-    int result;
-    int a = 0, b = 1;
-    
     if (num == 0){
         return 0;
     }
     if (num == 1){
         return 1;
     }
-
-    for (int i = 2; i <= num; i++){
-        result = a + b;
-        a = b;
-        b = result;
-    }
-    return result;
+    return Fibonacci(num - 2) + Fibonacci(num - 1);
 }
 
 int main(){
     int num;
-    
+
     cin >> num;
-    
+
     cout << Fibonacci(num);
 }
