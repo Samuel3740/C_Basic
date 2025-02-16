24170ab6bc9b3a5e1d57ecf42852370633693ef4
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-02-16 10:19:43 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string N;
+    
+    cin >> N;
+    
+    if(N == "0"){
+        cout << "YONSEI";
+    }
+    else{
+        cout << "Leading the Way to the Future";
+    }
+}
