165eae0a63cbedfa58ef6a28e6b50558ed21d1f3
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-02-17 09:09:04 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    
+    cin >> A;
+    cin >> B;
+    
+    int result = A * B; 
+    
+    cout << result << endl;
+    return 0;
+}
