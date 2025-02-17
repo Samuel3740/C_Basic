496a0762ed00659b030a56d26ad503890d035529
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-02-17 15:23:27 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,48 +1,27 @@
 #include <iostream>
 using namespace std;
 
-class Matrix{
-    int rows, cols;
-    int** data;
-
-public:
-    Matrix(int rows, int cols) : rows(rows), cols(cols){
-        data = new int*[rows];
-        for (int i = 0; i < rows; ++i) {
-            data[i] = new int[cols];
-        }
-    }
-
-    ~Matrix(){
-        for (int i = 0; i < rows; ++i){
-            delete[] data[i];
-        }
-        delete[] data;
-    }
-
-    void input() {
-        for (int i = 0; i < rows; ++i){
-            for (int j = 0; j < cols; ++j){
-                cin >> data[i][j];
-            }
-        }
-    }
-
-    void addAndPrint(const Matrix& other) const{
-        for (int i = 0; i < rows; ++i){
-            for (int j = 0; j < cols; ++j){
-                cout << data[i][j] + other.data[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
-};
+const int MAX = 100;
 
 int main(){
-    int N, M;
-    cin >> N >> M;
-    Matrix matrix1(N, M), matrix2(N, M);
-    matrix1.input();
-    matrix2.input();
-    matrix1.addAndPrint(matrix2);
+	int matrix[MAX][MAX] = {0};
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int n = 0; n < 2; ++n){
+		for (int i = 0; i < N; ++i){
+			for (int j = 0; j < M; ++j){
+				int num;
+				cin >> num;
+				matrix[i][j] += num;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; ++i){
+		for (int j = 0; j < M; ++j){
+			cout << matrix[i][j] << " ";}
+		cout << endl;
+	}
 }
