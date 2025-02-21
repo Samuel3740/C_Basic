78ba5d4e4dc0f8b3890ce56e764218178c12c27c
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-02-21 12:04:47 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -1,13 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-	int T; int A; int B;
+int main(){
+	int A, B, T;
+    
 	cin >> T;
     
-	for (int i = 1; i <= T; i++)
-	{
+	for (int i = 1; i < T + 1; i++){
 		cin >> A >> B;
 		cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << endl;
 	}
