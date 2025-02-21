70cffbe3a43c858334018ff799a763db466a5d39
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-02-21 16:21:25 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -3,16 +3,15 @@ using namespace std;
 
 int main(){
     int A, B, C, H, M;
-    cin >> A >> B >> C;
     
-    if(B + C > 59){
-        H = A + (B + C) / 60; 
-        M = (B + C) % 60;    
-        if(H > 23) H -= 24;   
+    cin >> A >> B;
+    cin >> C;
+    
+    M = (B + C) % 60;
+    H = A + ((B + C) / 60); 
+    
+    if(H > 23){
+        H -= 24;
     }
-    else {
-        H = A;
-        M = B + C;
-    }
-    cout << H << " " << M << endl;
+    cout << H << " " << M << endl;   
 }
