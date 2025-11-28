75e011067e53f21b559fff009fc351159b36d87c
Samuel <142777707+Samuel3740@users.noreply.github.com>
2025-11-28 18:37:12 +0900

    [Silver V] Title: 다리 놓기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,27 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+long long combination(int n, int r){
+    if(r > n) return 0;
+    if(r == 0 || r == n) return 1;
+    if(r > n - r) r = n - r;
+    
+    long long result = 1;
+
+    for(int i = 0; i < r; i++){
+        result *= (n - i);
+        result /= (i + 1);
+    }
+
+    return result;
+}
+
+
 int main(){
     int T;
     cin >> T;
-
-    for(int i = 0; i < T; ++i){
+    
+    while (T--){
         int N, M;
         cin >> N >> M;
         
-        long long dp[30][30] = {0};
-        
-        for(int j = 0; j <= M; j++){
-            dp[0][j] = 1;
-        }
-        
-        for(int j = 1; j <= N; j++){
-            for(int k = j; k <= M; k++){
-                dp[j][k] = dp[j][k-1] + dp[j-1][k-1];
-            }
-        }
-        
-        cout << dp[N][M] << endl;
+        cout << (N <= M ? combination(M, N) : 0) << endl;
     }
     
     return 0;
