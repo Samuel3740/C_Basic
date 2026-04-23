3517609757e59b9aa50134625bec5ffcb1b4b22c
Samuel <142777707+Samuel3740@users.noreply.github.com>
2026-04-23 16:30:17 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> num_list, int n) {    
+    int answer = 0;
+    
+    for(int i = 0; i < num_list.size(); i++){
+        if(num_list[i] == n){
+            answer = 1;
+            break;
+        }
+        else{
+            answer = 0;
+        }
+    }
+    return answer;
+}
+
+
+// int solution(vector<int> num_list, int n) {
+//     for(int i = 0; i < num_list.size(); i++){
+//         if(num_list[i] == n){
+//             return 1;
+//         }
+//     }
+//     return 0;
+// }
