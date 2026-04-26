176757e50b12c66b632585fbb269d5e0dee81a1b
Samuel <142777707+Samuel3740@users.noreply.github.com>
2026-04-26 23:47:18 +0900

    [level 0] Title: 특수문자 출력하기, Time: 2.17 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    
+    cout << "!@#$%^&*(\\\'\"<>?:;";
+    
+    return 0;
+}
