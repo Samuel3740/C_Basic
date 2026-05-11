249e050e674815a6225d4fcfd1851a7b6c62d176
Samuel <roysangjun@gmail.com>
2026-05-11 21:29:43 +0900

    [level 0] Title: 배열 만들기 2, Time: 1.17 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int l, int r) {
+    vector<int> answer;
+    
+    for (int i = l; i <= r; i++) {
+        int n =i;
+        bool ok = true;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            
+            if (digit != 0 && digit != 5) {
+                ok = false;
+                break;
+            }
+            n /= 10;
+        }
+        
+        if (ok) answer.push_back(i);
+    }
+    
+    if (answer.empty()) answer.push_back(-1);
+    
+    return answer;
+}
